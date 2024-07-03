121d817b279636ffe7e3eb3dcc10adf4d32593ac
senthilbalablockchain <senthil.bala@kaleidioscope-blockchain.com>
2024-07-03 17:04:29 +0530

    chore/Removed optimism packages

--- archive/optimism/packages/core-utils/src/external/geth/geth-types.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// Types explicitly related to dealing with Geth.
-Object.defineProperty(exports, "__esModule", { value: true });
--- archive/optimism/packages/sdk/tasks/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./deposit-eth");
-require("./deposit-erc20");
-require("./finalize-withdrawal");
